d5606bffb5831293ee9dc96a07649e31b037bddf
root <root@ip-10-42-128-88.ec2.internal>
2025-12-03 09:07:41 +0000

    fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for ch in my_string:
-        if ch != 'c' and ch != 'C':
-            new_string += ch
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
     return new_string
-
