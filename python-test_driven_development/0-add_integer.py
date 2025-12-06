fb1d87159663161abfb5b6f8de3f3ad43cb6c502
root <root@ip-10-42-209-113.ec2.internal>
2025-12-06 15:49:41 +0000

    add

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains the add_integer function
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers a and b.
+
+    a and b must be integers or floats
+    Floats are casted to integers before addition
+    Raises TypeError if a or b are not integers or floats
+    Returns the sum as an integer
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
