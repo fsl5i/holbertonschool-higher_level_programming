9db9f8ba49a35bd89dabd25e1bee21835a2c2736
root <root@ip-10-42-209-113.ec2.internal>
2025-12-06 15:25:33 +0000

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,10 +6,11 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-            except Exception:
+            except (TypeError, ValueError):
                 continue
-    except Exception:
+    except IndexError:
         pass
     finally:
         print()
     return count
+
