87cd6f10cd47659fbbaa79ac317309be7b9e8adc
root <root@ip-10-42-96-49.ec2.internal>
2025-12-07 12:07:17 +0000

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,22 +3,24 @@
 
 
 class Square:
-    """Represents a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-        
+        """Initialize a new Square.
+
         Args:
             size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
     def area(self):
         """Return the area of the square."""
-        return self.__size * self.__size
+        return self.__size ** 2
