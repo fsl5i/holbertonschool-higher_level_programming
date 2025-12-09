ca7e61a54c1047b51915f0ab35db646419efa028
root <root@ip-10-42-171-118.ec2.internal>
2025-12-09 12:53:10 +0000

    fix

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new rectangle.
-        
+
         Args:
             width (int): width of the rectangle
             height (int): height of the rectangle
