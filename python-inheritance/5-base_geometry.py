3890e0bde17f6470ace75e00e24b9f470d24f3ee
root <root@ip-10-42-134-63.ec2.internal>
2025-12-25 11:01:19 +0000

    add task5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that represents a base geometry.
+    """
+    pass
