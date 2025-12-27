3403b0d6f027f356991cf0d47d9e56ea595bc61e
root <root@ip-10-42-42-196.ec2.internal>
2025-12-27 10:45:46 +0000

    add task1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that contains a function to write text to a file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters.
+
+    Args:
+        filename (str): name of the file
+        text (str): text to write in the file
+
+    Returns:
+        int: number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
