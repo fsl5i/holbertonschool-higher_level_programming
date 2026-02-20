9f6e862f5b9b3334c251dddd140ffc35bdefa6ed
Faisal <busbar51.com@gmail.com>
2026-02-20 04:56:32 +0000

    Add MyInt class with inverted == and !=

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines MyInt class inheriting from int with inverted == and !="""
+
+class MyInt(int):
+    """MyInt: rebel integer with inverted equality"""
+
+    def __eq__(self, other):
+        """Invert == operator"""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """Invert != operator"""
+        return int(self) == other
