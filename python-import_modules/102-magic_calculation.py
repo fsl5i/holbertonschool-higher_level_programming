163117cabdd8b0851a1f77deec825b0e56b45a29
faisal <busbar51.com@gmail.com>
2026-02-21 17:30:28 +0000

    fix

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
+"""Module that contains magic_calculation function."""
+
+
+from magic_calculation_102 import add, sub
 
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
+    """Performs a magic calculation based on given bytecode."""
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return(sub(a, b))
+        return sub(a, b)
